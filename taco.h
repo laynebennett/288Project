d54d08aa69392110de99191477049e844dc35b43
Bennett <layneben@iastate.edu>
2025-05-02 17:49:40 -0500

    added taco.h

---
+++ taco.h
@@ -0,0 +1,33 @@
+/**
+ * taco.h
+ * Header file for taco.c
+ * Updates taco and delivery related information
+ */
+
+#include "movement.h"
+#include "open_interface.h"
+#include <inc/tm4c123gh6pm.h>
+#include <stdint.h>
+#include "ping.h"
+#include "Timer.h"
+#include "lcd.h"
+#include "driverlib/interrupt.h"
+#include<stdio.h>
+#include <stdbool.h>
+#include "adc.h"
+#include "servo.h"
+
+/**
+ * Logic for supplying the bot at home
+ */
+int void supplyBot()
+
+/**
+ * Logic for scanning customers and supplying customers
+ */
+int void supplyCustomers()
+
+/**
+ * Music library
+ */
+void music(int play_despacito, int play_home_jingle)
