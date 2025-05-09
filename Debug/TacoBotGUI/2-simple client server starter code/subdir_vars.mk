8b985c8a91af01e5021ef25b9e7edc7ed02f2353
Bennett <layneben@iastate.edu>
2025-05-08 21:31:57 -0500

    adsdaf

---
+++ Debug/TacoBotGUI/2-simple client server starter code/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../TacoBotGUI/2-simple\ client\ server\ starter\ code/Simple-CyBot-echo.c 
+
+C_DEPS += \
+./TacoBotGUI/2-simple\ client\ server\ starter\ code/Simple-CyBot-echo.d 
+
+OBJS += \
+./TacoBotGUI/2-simple\ client\ server\ starter\ code/Simple-CyBot-echo.obj 
+
+OBJS__QUOTED += \
+"TacoBotGUI\2-simple client server starter code\Simple-CyBot-echo.obj" 
+
+C_DEPS__QUOTED += \
+"TacoBotGUI\2-simple client server starter code\Simple-CyBot-echo.d" 
+
+C_SRCS__QUOTED += \
+"../TacoBotGUI/2-simple client server starter code/Simple-CyBot-echo.c" 
+
+
---
+++ Debug/TacoBotGUI/3-simple mock sensor client server starter code/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../TacoBotGUI/3-simple\ mock\ sensor\ client\ server\ starter\ code/Simple-CyBot-echo.c 
+
+C_DEPS += \
+./TacoBotGUI/3-simple\ mock\ sensor\ client\ server\ starter\ code/Simple-CyBot-echo.d 
+
+OBJS += \
+./TacoBotGUI/3-simple\ mock\ sensor\ client\ server\ starter\ code/Simple-CyBot-echo.obj 
+
+OBJS__QUOTED += \
+"TacoBotGUI\3-simple mock sensor client server starter code\Simple-CyBot-echo.obj" 
+
+C_DEPS__QUOTED += \
+"TacoBotGUI\3-simple mock sensor client server starter code\Simple-CyBot-echo.d" 
+
+C_SRCS__QUOTED += \
+"../TacoBotGUI/3-simple mock sensor client server starter code/Simple-CyBot-echo.c" 
+
+
